80ca6743d5e7c9f6c74fd2b4e8928da6f60a2795
Amir Dora <amirdora@gmail.com>
2022-02-03 11:06:05 +0500

    enabled viewbinding and navController links

--- app/src/main/java/com/digiclack/todoApp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/digiclack/todoApp/fragments/list/ListFragment.kt
@@ -5,17 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.digiclack.todoApp.R
+import com.digiclack.todoApp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
+    private lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(layoutInflater)
+
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return binding.root
     }
 
 }
